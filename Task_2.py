990fe2f5f722d32866cdb87db5eeadbe901352f9
Hamza Albanna <hamzaalbanna02@gmail.com>
2023-11-01 21:40:20 +0300

    task2

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+#loop 
+# The provided code stub reads and integer,n, from STDIN.
+# For all non-negative integers i<n, print (i^2)
+
+n=int(input("insert an integer number :"))
+
+
+for i in range(0,n):
+        print(i**2)
