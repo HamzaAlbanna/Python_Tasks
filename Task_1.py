c001991354634fe554602a20707844401211c4b8
Hamza Albanna <hamzaalbanna02@gmail.com>
2023-11-01 21:07:36 +0300

    task1

---
+++ Task_1.py
@@ -0,0 +1,8 @@
+#python Division
+
+a = int(input("Insert the first number  :"))
+b = int(input("Insert the second number :"))
+result=a/b
+
+print(f"The result of the integer division ({int(result)}) ")
+print(f"The result of the float division ({result}) ")
