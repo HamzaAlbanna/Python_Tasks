7c532c9f296e2f028d8559575da66e135a8a9003
Hamza Albanna <hamzaalbanna02@gmail.com>
2023-11-09 23:23:19 +0300

    task16

---
+++ Task_16.py
@@ -0,0 +1,13 @@
+def compareTriplets(a, b):
+    Ales=0
+    bobe=0
+    for i in range(len(a)):
+        if a[i]>b[i]:
+            Ales+=1
+        elif a[i]<b[i]:
+            bobe+=1    
+        
+        
+    return [Ales,bobe]      
+
+  
